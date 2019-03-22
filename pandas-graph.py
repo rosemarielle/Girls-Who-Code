c8373a1aa0683e01b43703d5846dd3379a2c3e52
Marielle Cunanan <mariellercunanan@gmail.com>
2019-03-21 22:42:03 -0700

    Add files via upload

---
+++ pandas-graph.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# Revenue values list
+revenue = [2.5, 3.5, 4.5, 5.5]
+
+# Operational costs list
+opCost = [1.5, 2.1, 2.5, 3.0]
+
+# Year value list
+year = [2013, 2014, 2015, 2016]
+
+revenueCostDF = pd.DataFrame(
+    {
+        "Revenue" : revenue,
+        "Operational Cost" : opCost,
+        "Year" : year
+    }
+
+)
+
+# Printing dataframe
+print(revenueCostDF)
+
+# Plot the revenue chart
+#revenueCostDF.plot(x="Year", y="Revenue")
+
+revenueCostDF.plot(
+    x="Year",
+    y=["Revenue", "Operational Cost"],
+    kind="barh",
+    title="Revenue vs Cost",
+    colormap="plasma")
+plt.show()
